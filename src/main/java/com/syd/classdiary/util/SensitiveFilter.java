b49386906376d5abf609e8448d5ba93ed4e5ed32
syd <syd199708@126.com>
2020-10-06 10:57:29 +0800

    开发敏感词过滤算法工具类

---
+++ src/main/java/com/syd/classdiary/util/SensitiveFilter.java
@@ -0,0 +1,177 @@
+package com.syd.classdiary.util;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class SensitiveFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    // 替换符
+    private static final String REPLACEMENT = "***";
+
+    // 根节点
+    private TrieNode rootNode = new TrieNode();
+
+    // @PostConstruct注解说明这是一个初始化方法，当容器调用这个bean实例化之后，这个方法自动被调用。
+    // 这个方法会在构造器之后调用
+    @PostConstruct
+    public void init() {
+        try (
+                // 字节流
+                InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                // 字符流
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        ) {
+            String keyword;
+            while ((keyword = reader.readLine()) != null) {
+                // 添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败: " + e.getMessage());
+        }
+    }
+
+    // 将一个敏感词添加到前缀树中
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode subNode = tempNode.getSubNode(c);
+
+            if (subNode == null) {
+                // 初始化子节点
+                subNode = new TrieNode();
+                tempNode.addSubNode(c, subNode);
+            }
+
+            // 指向子节点,进入下一轮循环
+            tempNode = subNode;
+
+            // 设置结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+    }
+
+    /**
+     * 过滤敏感词
+     *
+     * @param text 待过滤的文本
+     * @return 过滤后的文本
+     */
+    public String filter(String text){
+        if(StringUtils.isBlank(text)){
+            return null;
+        }
+        // 指针1
+        TrieNode tempNode = rootNode;
+        // 指针2
+        int begin = 0;
+        // 指针3
+        int position = 0;
+        // 结果
+        StringBuilder sb = new StringBuilder();
+
+        // 以指针2位条件比指针3更合适。以指针3作为循环条件，会遗漏结尾处的敏感词。
+        // eg:  敏感词有fabcd和abc。要核验的字符串的最后一段为 fabc。
+        // 此时指针二指f, 指针三指到c, 根据循环中的判断c的lastKeyword为false, position++, 此时跳出循环,
+        // 然后将fabc加到StringBuilder中, 但是abc这个敏感词没有被过滤掉.
+        while(begin < text.length()){
+            if(position < text.length()) {
+                Character c = text.charAt(position);
+
+                // 跳过符号
+                if (isSymbol(c)) {
+                    if (tempNode == rootNode) {
+                        begin++;
+                        sb.append(c);
+                    }
+                    position++;
+                    continue;
+                }
+
+                // 检查下级节点
+                tempNode = tempNode.getSubNode(c);
+                if (tempNode == null) {
+                    // 以begin开头的字符串不是敏感词
+                    sb.append(text.charAt(begin));
+                    // 进入下一个位置
+                    position = ++begin;
+                    // 重新指向根节点
+                    tempNode = rootNode;
+                }
+                // 发现敏感词
+                else if (tempNode.isKeywordEnd()) {
+                    // 发现敏感词,将begin~position字符串替换掉
+                    sb.append(REPLACEMENT);
+                    // 进入下一个位置
+                    begin = ++position;
+//                    // 重新指向根节点（可以加也可以不加），大佬没有加，我就不加了
+//                    tempNode = rootNode;
+                }
+                // 检查下一个字符
+                else {
+                    position++;
+                }
+            }
+            // position遍历越界仍未匹配到敏感词
+            else{
+                sb.append(text.charAt(begin));
+                position = ++begin;
+                tempNode = rootNode;
+            }
+        }
+        return sb.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // 0x2E80~0x9FFF 是东亚文字范围
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
+    }
+
+    // 前缀树
+    private class TrieNode {
+
+        // 关键词结束标识
+        private boolean isKeywordEnd = false;
+
+        // 子节点(key是下级字符,value是下级节点)
+        private Map<Character, TrieNode> subNodes = new HashMap<>();
+
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
+
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        // 添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        // 获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
+
+    }
+
+}
---
+++ src/test/java/com/syd/classdiary/SensitiveTests.java
@@ -0,0 +1,55 @@
+package com.syd.classdiary;
+
+import com.syd.classdiary.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = ClassdiaryApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博,可以嫖娼,可以吸毒,可以开票,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以☆赌☆博☆,可以☆嫖☆娼☆,可以☆吸☆毒☆,可以☆开☆票☆,哈哈哈!";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "fabcd";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "fabcc";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "fabc";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "☆f☆a☆b☆c☆f☆a☆b☆c☆";
+//        text = "☆a☆b☆c☆";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "☆f☆a☆b☆c";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "f☆a☆b☆c";
+        text  =  sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+
+}
