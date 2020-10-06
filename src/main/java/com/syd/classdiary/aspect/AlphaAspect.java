b74e6088e3ea541c50ad796c8a8bbbd4b6b2624c
syd <syd199708@126.com>
2020-10-06 22:02:25 +0800

    利用aop技术实现统一记录日志功能

---
+++ src/main/java/com/syd/classdiary/aspect/AlphaAspect.java
@@ -0,0 +1,49 @@
+package com.syd.classdiary.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+//@Component
+//@Aspect
+public class AlphaAspect {
+
+    @Pointcut("execution(* com.syd.classdiary.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    // 连接点之前
+    @Before("pointcut()")
+    public void before() {
+        System.out.println("before");
+    }
+
+    // 连接点之后
+    @After("pointcut()")
+    public void after() {
+        System.out.println("after");
+    }
+
+    // 返回值以后
+    @AfterReturning("pointcut()")
+    public void afterRetuning() {
+        System.out.println("afterRetuning");
+    }
+
+    // 抛异常之后
+    @AfterThrowing("pointcut()")
+    public void afterThrowing() {
+        System.out.println("afterThrowing");
+    }
+
+    // 环绕   在之前处理，也在之后处理
+    @Around("pointcut()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        System.out.println("around before");
+        Object obj = joinPoint.proceed();
+        System.out.println("around after");
+        return obj;
+    }
+
+}
---
+++ src/main/java/com/syd/classdiary/aspect/ServiceLogAspect.java
@@ -0,0 +1,43 @@
+package com.syd.classdiary.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+@Aspect
+public class ServiceLogAspect {
+
+    private static final Logger logger = LoggerFactory.getLogger(ServiceLogAspect.class);
+
+    @Pointcut("execution(* com.syd.classdiary.service.*.*(..))")
+    public void pointcut() {
+
+    }
+
+    @Before("pointcut()")
+    public void before(JoinPoint joinPoint) {
+        // 用户[1.2.3.4],在[xxx],访问了[com.syd.community.service.xxx()].
+        // 这里不能直接在上面添加  HttpServletRequest request，需要采用如下方面得到request对象。
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes == null) {  //通过消费者去调的，此时我们简单处理不去记录日志了。
+            return;
+        }
+        HttpServletRequest request = attributes.getRequest();
+        String ip = request.getRemoteHost();
+        String now = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String target = joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName();
+        logger.info(String.format("用户[%s],在[%s],访问了[%s].", ip, now, target));
+    }
+
+}
